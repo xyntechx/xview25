e3e88feb8075bd99feacf7863b9b7d2ef92f588e
xyntechx <xyntechx@gmail.com>
2025-05-01 15:59:08 +0000

    ConvNextV2 backbone + UperNet

--- train.py
+++ train.py
@@ -1,9 +1,7 @@
 import torch
 import torch.nn.functional as F
 from torchgeo.datasets import XView2
-from torchvision.transforms.functional import convert_image_dtype
-from transformers import UperNetForSemanticSegmentation, TrainingArguments, Trainer
-import evaluate
+from transformers import ConvNextImageProcessorFast, UperNetConfig, UperNetForSemanticSegmentation, TrainingArguments, Trainer
 
 
 def collate_fn(examples):
@@ -11,59 +9,65 @@ def collate_fn(examples):
     for pair in examples:
         image_pair, mask_pair = pair["image"], pair["mask"]
         for image in image_pair:
-            images.append(convert_image_dtype(image))
+            processed_image = image_processor.preprocess(image, return_tensors="pt")["pixel_values"][0]
+            images.append(processed_image)
         for mask in mask_pair:
-            masks.append(mask)
+            interpolated = F.interpolate(mask.float().unsqueeze(0).unsqueeze(0), size=(224, 224), mode='nearest')
+            masks.append(torch.round(interpolated).squeeze().long())
+    pixel_values = torch.stack(images)
+    labels = torch.stack(masks)
+    return {"pixel_values": pixel_values, "labels": labels}
 
-    # images = torch.stack([example["image"].to(memory_format=torch.channels_last) for example in examples])
-    # masks = torch.stack([example["mask"] for example in examples])
-    return {"image": torch.stack(images), "mask": torch.stack(masks)}
-
-
-class MyTrainer(Trainer):
-    def compute_loss(self, model, inputs, num_items_in_batch=None, return_outputs=False):
-        outputs = model(inputs["image"])
-        target = inputs["mask"]
-        loss = F.binary_cross_entropy(F.sigmoid(outputs), target)
-        return (loss, outputs) if return_outputs else loss
-
-
-model = UperNetForSemanticSegmentation.from_pretrained("openmmlab/upernet-convnext-tiny")
-model.config.id2label = {
-    0: "background",
-    1: "no damage",
-    2: "minor damage",
-    3: "major damage",
-    4: "destroyed"
-}
-model.config.label2id = {
-    "background": 0,
-    "no damage": 1,
-    "minor damage": 2,
-    "major damage": 3,
-    "destroyed": 4
-}
+image_processor = ConvNextImageProcessorFast(do_resize=True, size=224, return_tensors="pt")
+config = UperNetConfig(
+    backbone="facebook/convnextv2-tiny-1k-224",
+    use_pretrained_backbone=True,
+    backbone_kwargs={
+        "out_features": ["stage1", "stage2", "stage3", "stage4"],
+    },
+    num_labels=5,
+    id2label={
+        0: "background",
+        1: "no damage",
+        2: "minor damage",
+        3: "major damage",
+        4: "destroyed"
+    },
+    label2id = {
+        "background": 0,
+        "no damage": 1,
+        "minor damage": 2,
+        "major damage": 3,
+        "destroyed": 4
+    }
+)
+model = UperNetForSemanticSegmentation(config)
 
 training_args = TrainingArguments(
+    run_name="xview25",
     output_dir="output",
-    per_device_train_batch_size=64,
-    per_device_eval_batch_size=64,
+    per_device_train_batch_size=4,
+    per_device_eval_batch_size=4,
     num_train_epochs=10,
     eval_strategy="epoch",
     remove_unused_columns=False,
     dataloader_num_workers=1,
+    ddp_find_unused_parameters=False,
+    ddp_backend="nccl",
 )
 
 DATA_PATH = "../../../datasets/xview2/current"
 train_ds = XView2(DATA_PATH, split="train")
 test_ds = XView2(DATA_PATH, split="test")
 
-trainer = MyTrainer(
+trainer = Trainer(
     model,
     training_args,
     train_dataset=train_ds,
     eval_dataset=test_ds,
     data_collator=collate_fn,
 )
+# TODO: HF trainer wandb logging
 
 trainer.train()
+trainer.save_model("model")
